3decb9b0bfc7f9f1d08021c27f568c0f4ba5796b
kchen <kennyrocker82@gmail.com>
2014-08-11 13:52:29 -0400

    reformat dataStructure.js

--- dataStructure.js
+++ dataStructure.js
@@ -1,65 +1,49 @@
 // User
-
-	{
-		"userID":1001,
-		"userName":"Kennyrocker",
-		"associatedUserID":[
-								1002,1005,1006,1007
-						   ]				   
-
-	}
-
-
-
+{
+	"userID": 1001,
+	"userName": "Kennyrocker",
+	"associatedUserID": [
+		1002, 1005, 1006, 1007
+	]
+}
 // Note Assignment
-
-	{
-		"noteID":101,
-		"ownerID":1001,
-		"assigneeID":1002
-	}
-
-
-
+{
+	"noteID": 101,
+	"ownerID": 1001,
+	"assigneeID": 1002
+}
 // Note Meta
-
-	{
-		"noteID":101,
-		"startDate":20140303023023,
-		"lastModifyDate":20140303023023,
-		"status":0,
-		"lastModifyByUserID":1001,
-		"noteCategories":1,
-	}
-
-
-
+{
+	"noteID": 101,
+	"startDate": 20140303023023,
+	"lastModifyDate": 20140303023023,
+	"status": 0,
+	"lastModifyByUserID": 1001,
+	"noteCategories": 1,
+}
 // Note Content
-
-	{
-		"noteID":101,
-		"title":"Sticky",
-		"description":"Sticky Rocks",
-		"links":[
-					"http://www.sample.com",
-					"https://domain.ca/somelink/#"
-				],
-		"screenshoot":[
-						  "sample.jpg",
-						  "screenshoot.png"
-					  ],
-		"attachment":[
-						 "attachment.pdf",
-						 "attachment.doc"
-					 ],
-		"audio":[
-					"song.mp3",
-					"anothersong.wav"
-				],
-		"video":[
-					"video.mpeg",
-					"video.avi"
-				]					 			  
-	}
-
-
+{
+	"noteID": 101,
+	"title": "Sticky",
+	"description": "Sticky Rocks",
+	"links": [
+		"http://www.sample.com",
+		"https://domain.ca/somelink/#"
+	],
+	"screenshoot": [
+		"sample.jpg",
+		"screenshoot.png"
+	],
+	"attachment": [
+		"attachment.pdf",
+		"attachment.doc"
+	],
+	"audio": [
+		"song.mp3",
+		"anothersong.wav"
+	],
+	"video": [
+		"video.mpeg",
+		"video.avi"
+	]
+}
